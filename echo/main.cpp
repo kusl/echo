9d9fede2b23df3de33864e68217c4d4ede588703
stem1 <stem1@stem1.local>
2014-04-04 15:27:31 -0500

    Initial Commit

---
+++ echo/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  echo
+//
+//  Created by stem1 on 4/4/14.
+//  Copyright (c) 2014 kushal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
